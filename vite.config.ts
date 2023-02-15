b57b78bc25de6ca38c6760b3c7813f37a480cc98
roo12589 <15033341423@163.com>
2023-02-15 15:40:03 +0800

    chore: 安装visualizer

--- vite.config.ts
+++ vite.config.ts
@@ -3,14 +3,17 @@ import vue from '@vitejs/plugin-vue'
 import { wrapperEnv } from './src/utils/index'
 import path from 'path'
 import vueSetupExtend from 'vite-plugin-vue-setup-extend'
+import { visualizer } from 'rollup-plugin-visualizer'
+import { UserConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd())
     const viteEnv = wrapperEnv(env)
-
+    const isProduction = false
+    console.log("env,mode",env,mode);
     const { VITE_PORT, VITE_PROXY, VITE_PUBLIC_PATH } = viteEnv as any
-    return {
+    const config: UserConfig = {
         plugins: [vue(), vueSetupExtend()],
         base: VITE_PUBLIC_PATH || '',
         define: {
@@ -41,4 +44,15 @@ export default defineConfig(({ command, mode }) => {
             },
         },
     }
+    if (isProduction) {
+        config.plugins.push(
+            // 打包优化-可视化视图
+            visualizer({
+                open: true,
+                gzipSize: true, //搜集gzip压缩包的大小到图表
+                brotliSize: true, //搜集brotli压缩包的大小到图表
+            })
+        )
+    }
+    return config
 })
